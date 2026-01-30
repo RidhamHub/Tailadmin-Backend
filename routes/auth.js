8f5f7eda3f222af04031eae7a478fa6147330b09
Ridham Kakadiya <ridhamkakadiya2005@gmail.com>
2026-01-30 14:30:01 +0530

    check

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const upload = require("../middleware/uploadMiddleware");
 
 
 router.post("/signup", upload.single("profileImage"), createUser)
-router.post("/login", handleLoginUser)
+router.post("/login", handleLoginUser) 
 router.post("/logout", handleLogout)
 router.post('/refresh', handleRefershToken); // 401 ni bad request ave to aa call karavi levanu frontend mathi , so accessToken cretae thay jay authomatically....
 
